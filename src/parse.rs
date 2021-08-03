ac9dbdef02422e9d5cef3a902e516430a44dc999
yicuiheng <yicuiheng@gmail.com>
2021-08-03 23:25:30 +0900

    parse ifz-expr and let-expr

--- src/parse.rs
+++ src/parse.rs
@@ -105,6 +105,8 @@ fn primary_expr_from_pair(pair: pest::iterators::Pair<Rule>) -> Result<Expr<PosI
     let pair = pairs_iter.next().unwrap();
 
     match pair.as_rule() {
+        Rule::ifz_expr => ifz_expr_from_pair(pair),
+        Rule::let_expr => let_expr_from_pair(pair),
         Rule::constant => constant_from_pair(pair).map(|c| Expr::Constant(c)),
         Rule::variable => ident_from_pair(pair).map(|ident| Expr::Var(ident)),
         Rule::left_paren => {
@@ -117,6 +119,61 @@ fn primary_expr_from_pair(pair: pest::iterators::Pair<Rule>) -> Result<Expr<PosI
         }
         _ => unreachable!(),
     }
+}
+
+fn ifz_expr_from_pair(pair: pest::iterators::Pair<Rule>) -> Result<Expr<PosInfo>, ParseError> {
+    let pos_info = span_to_pos_info(&pair.as_span());
+    let mut pairs_iter = pair.into_inner();
+
+    assert_eq!(pairs_iter.next().unwrap().as_rule(), Rule::kw_ifz);
+
+    let cond_pair = pairs_iter.next().unwrap();
+    assert_eq!(cond_pair.as_rule(), Rule::expr);
+    let cond = expr_from_pair(cond_pair)?;
+
+    assert_eq!(pairs_iter.next().unwrap().as_rule(), Rule::kw_then);
+
+    let expr1_pair = pairs_iter.next().unwrap();
+    assert_eq!(expr1_pair.as_rule(), Rule::expr);
+    let expr1 = expr_from_pair(expr1_pair)?;
+
+    assert_eq!(pairs_iter.next().unwrap().as_rule(), Rule::kw_else);
+
+    let expr2_pair = pairs_iter.next().unwrap();
+    assert_eq!(expr2_pair.as_rule(), Rule::expr);
+    let expr2 = expr_from_pair(expr2_pair)?;
+
+    Ok(Expr::Ifz(
+        Box::new(cond),
+        Box::new(expr1),
+        Box::new(expr2),
+        pos_info,
+    ))
+}
+
+fn let_expr_from_pair(pair: pest::iterators::Pair<Rule>) -> Result<Expr<PosInfo>, ParseError> {
+    let pos_info = span_to_pos_info(&pair.as_span());
+    let mut pairs_iter = pair.into_inner();
+
+    assert_eq!(pairs_iter.next().unwrap().as_rule(), Rule::kw_let);
+
+    let ident_pair = pairs_iter.next().unwrap();
+    assert_eq!(ident_pair.as_rule(), Rule::ident);
+    let ident = ident_from_pair(ident_pair)?;
+
+    assert_eq!(pairs_iter.next().unwrap().as_rule(), Rule::equal);
+
+    let expr1_pair = pairs_iter.next().unwrap();
+    assert_eq!(expr1_pair.as_rule(), Rule::expr);
+    let expr1 = expr_from_pair(expr1_pair)?;
+
+    assert_eq!(pairs_iter.next().unwrap().as_rule(), Rule::kw_in);
+
+    let expr2_pair = pairs_iter.next().unwrap();
+    assert_eq!(expr2_pair.as_rule(), Rule::expr);
+    let expr2 = expr_from_pair(expr2_pair)?;
+
+    Ok(Expr::Let(ident, Box::new(expr1), Box::new(expr2), pos_info))
 }
 
 fn constant_from_pair(pair: pest::iterators::Pair<Rule>) -> Result<Constant<PosInfo>, ParseError> {
@@ -186,6 +243,79 @@ fn test() {
                 info: PosInfo { start: 9, end: 10 }
             })),
             PosInfo { start: 1, end: 10 }
+        )
+    );
+
+    assert_eq!(
+        expr("1 + ifz a then 41 else 90 + 9").unwrap(),
+        Expr::BinApp(
+            BinOp::Add(PosInfo { start: 2, end: 3 }),
+            Box::new(Expr::Constant(Constant {
+                val: 1,
+                info: PosInfo { start: 0, end: 1 }
+            })),
+            Box::new(Expr::Ifz(
+                Box::new(Expr::Var(Ident {
+                    name: "a".to_string(),
+                    info: PosInfo { start: 8, end: 9 }
+                })),
+                Box::new(Expr::Constant(Constant {
+                    val: 41,
+                    info: PosInfo { start: 15, end: 17 }
+                })),
+                Box::new(Expr::BinApp(
+                    BinOp::Add(PosInfo { start: 26, end: 27 }),
+                    Box::new(Expr::Constant(Constant {
+                        val: 90,
+                        info: PosInfo { start: 23, end: 25 }
+                    })),
+                    Box::new(Expr::Constant(Constant {
+                        val: 9,
+                        info: PosInfo { start: 28, end: 29 }
+                    })),
+                    PosInfo { start: 23, end: 29 }
+                )),
+                PosInfo { start: 4, end: 29 }
+            )),
+            PosInfo { start: 0, end: 29 }
+        )
+    );
+
+    assert_eq!(
+        expr("let a = 1 in let b = 2 in a + b").unwrap(),
+        Expr::Let(
+            Ident {
+                name: "a".to_string(),
+                info: PosInfo { start: 4, end: 5 }
+            },
+            Box::new(Expr::Constant(Constant {
+                val: 1,
+                info: PosInfo { start: 8, end: 9 }
+            })),
+            Box::new(Expr::Let(
+                Ident {
+                    name: "b".to_string(),
+                    info: PosInfo { start: 17, end: 18 }
+                },
+                Box::new(Expr::Constant(Constant {
+                    val: 2,
+                    info: PosInfo { start: 21, end: 22 }
+                })),
+                Box::new(Expr::BinApp(
+                    BinOp::Add(PosInfo { start: 28, end: 29 }),
+                    Box::new(Expr::Var(Ident {
+                        name: "a".to_string(),
+                        info: PosInfo { start: 26, end: 27 }
+                    })),
+                    Box::new(Expr::Var(Ident {
+                        name: "b".to_string(),
+                        info: PosInfo { start: 30, end: 31 }
+                    })),
+                    PosInfo { start: 26, end: 31 }
+                )),
+                PosInfo { start: 13, end: 31 }
+            )),
+            PosInfo { start: 0, end: 31 }
         )
     );
 }
